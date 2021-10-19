1c89ef68cb829a5cf04f460ed3ebe5317165faf9
Fabian Segatz <fabian.segatz@gmail.com>
2021-10-19 21:53:46 +0200

    EventPlanning: Write test

---
+++ models/eventplanning.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+class EventPlanning:
+    def __init__(self, client_record_number, client_name, description, expected_attendees, planned_budget, start_date, end_date):
+        self.id = ""
+        self.creation_date = ""
+        self.client_record_number = ""
+        self.client_name = ""
+        self.event_type = ""
+        self.description = ""
+        self.expected_attendees = ""
+        self.planned_budget = ""
+        self.start_date = ""
+        self.end_date = ""
+        self.info_decoration = ""
+        self.info_catering = ""
+        self.info_documentation = ""
+        self.info_music = ""
+        self.info_graphic = ""
+        self.info_technical = ""
+        self.info_other = ""
+        self.feasibility_review = ""
+        self.financial_review  = ""
+        self.status = ""
+        return
