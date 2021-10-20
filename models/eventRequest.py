bde5c1da5286545530db11d9e1e7f0ba7ba40259
Erik Wendel <ewend@kth.se>
2021-10-20 07:03:57 +0200

    fixed add info outside constructor

--- models/eventRequest.py
+++ models/eventRequest.py
@@ -9,7 +9,13 @@ from clear import clear
 
 class EventRequest():
 
-    def __init__(self):
+    def __init__(self,id):
+        self.__id=id
+    def add_info(self):
+        self.__feasibility_review=None
+        self.__financial_review=None
+        self.__assigned2="SCSO"
+
         clear()
         print("Please enter folowing details about the Request")
         self.__client_record_number=input("Client record number: ")
@@ -23,10 +29,31 @@ class EventRequest():
         self.__status=input("Status: ")
         input("Event request completed! Press enter to continue")
 
-        self.__feasibility_review=None
-        self.__financial_review=None
+    def add_info_test(self, atributes):
+        self.__assigned2="SCSO"
+        self.__client_record_number=atributes[0]
+        self.__client_name=atributes[1]
+        self.__event_type=atributes[2]
+        self.__expected_number_attendees=atributes[3]
+        self.__expected_budget=atributes[4]
+        self.__start_date=atributes[5]
+        self.__end_date=atributes[6]
+        self.__priorities=atributes[7]
+        self.__status=atributes[8]
+
+    def get_id(self):return self.__id
+    def get_name(self):return self.__id
+    def get_feasabilty_review(self): return self.__feasibility_review
+    def get_assigned2(self):return self.__assigned2
 
 class EventRequest_control():
     def create_eventRequest(self):
-        database.eventRequest_List.append(EventRequest())
+        e=EventRequest(database.id_counter.get_new())
+        e.add_info()
+        database.eventRequest_List.append(e)
 
+    def create_eventRequest_test(self,atributes):
+        e=EventRequest(database.id_counter.get_new())
+        e.add_info_test(atributes)
+        database.eventRequest_List.append(e)
+        #database.eventRequest_List.append(EventRequest(database.id_counter.get_new()).add_info_test(atributes))
