d8bc576ca1adcaa2f3f38fa884f5f7640ee5473e
Fabian Segatz <fabian.segatz@gmail.com>
2021-10-22 13:06:05 +0200

    Eventplanning: Add feature to main system

--- ui/pmView.py
+++ ui/pmView.py
@@ -1,4 +1,5 @@
 from clear import clear
+from models.eventplanning import EventPlanning_Control
 from models.subteamtask import *
 import time
 
@@ -9,6 +10,7 @@ def pm_view():
         print("------------------------")
         print("[0] Logout")
         print("[1] Create subteam task")
+        print("[2] Show current events")
         option = input("Please choose option: ")
         if(option == '0'):
             break
@@ -16,4 +18,9 @@ def pm_view():
             clear()
             subTeamTask_Control = SubTeamTask_Control()
             tmp = subTeamTask_Control.create_subteamtask_form()
+        if(option == '2'):
+            clear()
+            eventPlanning_Control = EventPlanning_Control()
+            tmp = eventPlanning_Control.show_current_eventplannings()
+            input('Press enter to return') #temporary before adding functionality
     return
--- ui/smView.py
+++ ui/smView.py
@@ -1,5 +1,6 @@
 from clear import clear
 from models.subteamtask import *
+from models.eventplanning import *
 import time
 
 def sm_view():
@@ -16,4 +17,9 @@ def sm_view():
             clear()
             subTeamTask_Control = SubTeamTask_Control()
             tmp = subTeamTask_Control.create_subteamtask_form()
+        if(option == '2'):
+            clear()
+            eventPlanning_Control = EventPlanning_Control()
+            tmp = eventPlanning_Control.show_current_eventplannings()
+            input('Press enter to return') #temporary before adding functionality
     return
