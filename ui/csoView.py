fb6a06c427f2cdfc9212d503dd84c41d5974c5ae
Erik Wendel <ewend@kth.se>
2021-10-22 21:53:15 +0200

    fixed CSO view to class

--- ui/csoView.py
+++ ui/csoView.py
@@ -8,36 +8,40 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, parentdir)))
 
 from clear import clear
 
-def cso_view():
-    while True:
+class CustomerSupportOfficerView():
+    def __init__(self):
+        self.eventRequest_control=EventRequest_control()
+        while True:
+            key=self.start_view()
+
+            if key=="1":self.create_event_request()
+                
+            elif key=="2": return
+
+    def start_view(self): 
         clear()
-        key=input("\
-Customer Support Officer Main View\n\
-------------------------\n\
-[1] Create Event Request \n\
-[2] Logout\n")
+        print("Customer Support Officer Main View")
+        print("------------------------")
+        print("[1] Create Event Request")
+        print("[2] Logout")
+        return input()
 
-        if key=="1":
-            clear()
-            print("Please enter folowing details about the Request")
-            client_record_number=input("Client record number: ")
-            client_name=input("Client_name: ")
-            event_type=input("Event_type: ")
-            expected_number_attendees=input("Expected number attendees: ")
-            expected_budget=input("Expected budget: ")
-            start_date=input("Start date: ")
-            end_date=input("End date: ")
-            priorities=input("Priorities: ")
+    def create_event_request(self):
+        clear()
+        print("Please enter folowing details about the Request")
+        client_record_number=input("Client record number: ")
+        client_name=input("Client_name: ")
+        event_type=input("Event_type: ")
+        expected_number_attendees=input("Expected number attendees: ")
+        expected_budget=input("Expected budget: ")
+        start_date=input("Start date: ")
+        end_date=input("End date: ")
+        priorities=input("Priorities: ")
 
-            atributes=[client_record_number, client_name, event_type, expected_number_attendees, \
-            expected_budget, start_date, end_date, priorities]
+        atributes=[client_record_number, client_name, event_type, expected_number_attendees, \
+        expected_budget, start_date, end_date, priorities]
 
-            try:eventRequest_control
-            except:eventRequest_control=EventRequest_control()
-            eventRequest_control.create_eventRequest(atributes)
-            
-            input("Event request completed! Press enter to continue")
-            
-        elif key=="2":
-            return
-    return 
+
+        self.eventRequest_control.create_eventRequest(atributes)
+        
+        input("Event request completed! Press enter to continue")
