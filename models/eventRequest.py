4b90324794c9fb94313500081892dc8d43b4ba08
Erkiperkki <92603226+Erkiperkki@users.noreply.github.com>
2021-10-22 14:14:47 +0200

    Close #8 Eventplanning initiate dev (#38)
    * fixed small func for event request
    
    * fixed test file for user story
    
    * added func to main
    
    Co-authored-by: Erik Wendel <ewend@kth.se>

--- models/eventRequest.py
+++ models/eventRequest.py
@@ -29,15 +29,17 @@ class EventRequest():
 
     def get_id(self):return self.__id
     def get_name(self):return self.__id
+    def get_status(self): return self.__status
     def get_feasability_review(self): return self.__feasibility_review
     def get_financial_review(self):return self.__financial_review
     def get_assigned2(self):return self.__assigned2
+    def get_all_data(self):return [self.__client_record_number, self.__client_name, self.__event_type, self.__expected_number_attendees, \
+            self.__expected_budget, self.__start_date, self.__end_date, self.__priorities, self.__assigned2, self.__status, self.__feasibility_review, self.__financial_review]
 
     def set_feasabilty_review(self, review):self.__feasibility_review=review
     def set_financial_review(self, review):self.__financial_review=review
     def set_assigned2(self, user): self.__assigned2=user
     def set_status(self, status): self.__status=status
-
 
 class EventRequest_control():
     def create_eventRequest(self,atributes, assigned2="SCSO", status="Under review", feasibility_review="", financial_review=""):
@@ -45,49 +47,44 @@ class EventRequest_control():
         e.add_info(atributes, assigned2, status, feasibility_review, financial_review)
         database.eventRequest_List.append(e)
 
-    def get_event_request_for_user(self): return [str(req.get_id()) for req in database.eventRequest_List if req and req.get_assigned2()==database.currentUser.position]
-    
-    def add_review(self, id, review): 
-        requests=[req for req in database.eventRequest_List if req.get_id()==int(id)]
-        
-        if database.currentUser.position=="SCSO":
-            requests[0].set_feasabilty_review(review)
-            requests[0].set_assigned2("FM")
-        
-        elif database.currentUser.position=="FM":
-            requests[0].set_financial_review(review)
-            requests[0].set_assigned2("AM")
-        
+    def get_finalized_event_request(self): return [req.get_id() for req in database.eventRequest_List if req and req.get_status()=="Finalized"]
+    def get_event_request_for_user(self): return [req.get_id() for req in database.eventRequest_List if req and req.get_assigned2()==database.currentUser.position]
+    def get_event_request_from_id(self,id): return [req for req in database.eventRequest_List if req and req.get_id()==id][0]
+    def get_info_for_event_plan(self, id): return [req.get_all_data() for req in database.eventRequest_List if req and req.get_id()==id][0]
+
     def get_reviews_from_event_request(self, id):
         for req in database.eventRequest_List:
             if req.get_id()==id:
                 return req.get_feasability_review(), req.get_financial_review()
-
+    
     def add_review(self, id, review): 
-        requests=[req for req in database.eventRequest_List if req.get_id()==id]
-
+        request=self.get_event_request_from_id(id)
+        
         if database.currentUser.position=="SCSO":
-            requests[0].set_feasabilty_review(review)
-            requests[0].set_assigned2("FM")
+            request.set_feasabilty_review(review)
+            request.set_assigned2("FM")
         
         elif database.currentUser.position=="FM":
-            requests[0].set_financial_review(review)
-            requests[0].set_assigned2("AM")
-
+            request.set_financial_review(review)
+            request.set_assigned2("AM")
+        
     def reject_event_request(self, id):
         for i,req in enumerate(database.eventRequest_List):
             if req.get_id()==id: 
                 req.set_status("Rejected")
-                database.eventRequest_List.pop(i)
+                req.set_assigned2("")
+                #database.eventRequest_List.pop(i)
                 return
 
     def finalize_event_request(self, id):
-        requests=[req for req in database.eventRequest_List if req.get_id()==id]
-        requests[0].set_status("Finalized")
-        requests[0].set_assigned2("")
+        request=self.get_event_request_from_id(id)
+        request.set_status("Finalized")
+        request.set_assigned2("")
 
-    def create_eventRequest_test(self,atributes):
-        e=EventRequest(database.id_counter.get_new())
-        e.add_info(atributes)
-        database.eventRequest_List.append(e)
-
+    def archived_event_request(self, id): 
+        for i,req in enumerate(database.eventRequest_List):
+            if req.get_id()==id: 
+                req.set_status("Archived")
+                req.set_assigned2("")
+                #database.eventRequest_List.pop(i)
+                return
--- ui/scsoView.py
+++ ui/scsoView.py
@@ -5,6 +5,7 @@ parentdir = '..'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, parentdir)))
 
 from models.eventRequest import EventRequest_control
+from models.eventplanning import *
 from clear import clear 
 import database
 
@@ -14,7 +15,8 @@ def scso_view():
         print("Senior Customer Support Officer Main View")
         print("------------------------")
         print("[1] Show Event Request to review")
-        print("[2] Logout")
+        print("[2] Create event plan from event request")
+        print("[0] Logout")
         key=input()
 
         if key=="1":
@@ -33,8 +35,35 @@ def scso_view():
                     if key=="0":pass
                     else:eventRequest_control.add_review(id=req_id, review=key)
                     
-
         elif key=="2":
+                    while True:
+                        try:eventRequest_control
+                        except:eventRequest_control=EventRequest_control()
+
+                        clear()
+                        print("All finalized event requests")
+                        event_requests=eventRequest_control.get_finalized_event_request()
+                        print(*event_requests)
+                        req_id=input("Press id of event request to create event plan from or press [0] to go back: ")
+                        if req_id == "0":break
+                        elif req_id in event_requests:
+                            clear()
+                            print("Are you sure that you want to create event plan from event request " + req_id + " it will not be possible to undo")
+                            print("[1] Yes, create event plan")
+                            print("[2] No, go back")
+                            key=input("")
+                            if key=="1":
+                                event_request_data=eventRequest_control.get_info_for_event_plan(id=req_id)
+                                eventRequest_control.archived_event_request(id=req_id)
+
+                                try:eventplanning_control
+                                except:eventplanning_control=EventPlanning_Control()
+
+                                eventplanning=EventPlanning(event_request_data[0], event_request_data[1], event_request_data[2], event_request_data[3], event_request_data[4], event_request_data[5], event_request_data[6], event_request_data[7])
+                                #this should probobly be done in the control object not in boundary to keep more seperated 
+                                eventplanning_control.append(eventplanning)
+
+        elif key=="0":
             return
             
         elif key=="test":
@@ -42,4 +71,6 @@ def scso_view():
             try:eventRequest_control
             except:eventRequest_control=EventRequest_control()
             eventRequest_control.create_eventRequest(atributes)
+        
+
     return 
