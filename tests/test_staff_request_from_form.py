f0e61dc495c35af0dd89f92555c88e79263937d4
Erik Wendel <ewend@kth.se>
2021-10-23 14:16:00 +0200

    imnplemnted to pm view as well

--- tests/test_staff_request_from_form.py
+++ tests/test_staff_request_from_form.py
@@ -6,13 +6,18 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, parentdir)))
 from models.staffrequest import *
 import database
 from ui.smView import *
+from ui.pmView import *
 
 
 def main():
     database.initialize()
 
     ServiceManagerView().create_staff_request() #start with enter 0
-    if database.staffRequest_List: print("it worked!")
-    else: print("it did not work!! :,(")
+    if database.staffRequest_List: print("it worked with ServiceManagerView!")
+    else: print("it did not work with ServiceManagerView!! :,(")
+
+    ProductionManagerView().create_staff_request() #start with enter 0
+    if database.staffRequest_List: print("it worked with ProductionManagerView!")
+    else: print("it did not work with ProductionManagerView!! :,(")
 
 main()
