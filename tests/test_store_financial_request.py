dea71f3b8f949ce489c85fce71c69357b468aec3
Erik Wendel <ewend@kth.se>
2021-10-23 19:37:50 +0200

    forgot to change obj name, gave false positiv

--- tests/test_store_financial_request.py
+++ tests/test_store_financial_request.py
@@ -3,7 +3,7 @@ testdir = os.path.dirname(__file__)
 parentdir = '..'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, parentdir)))
 
-from models.staffrequest import *
+from models.financialrequest import *
 import database
 
 
@@ -12,9 +12,9 @@ def main():
 
     atributes=["id 192", "Decoration" , "40 000" , "Need to buy new chandalier" , "Plz u owe me one", "Opened"]
 
-    req=StaffRequest(database.id_counter.get_new(),atributes)
-    database.staffRequest_List.append(req)
-    if req in database.staffRequest_List: print("it worked!")
+    req=FinancialRequest(database.id_counter.get_new(),atributes)
+    database.financialRequest_List.append(req)
+    if req in database.financialRequest_List: print("it worked!")
     else: print("it did not work!! :,(")
 
 main()
