d85ce56204095d87ef638cf2a6c00769184218bb
Erik Wendel <ewend@kth.se>
2021-10-23 15:21:57 +0200

    implemnted see staff requests from hrtm view

---
+++ ui/hrtmView.py
@@ -0,0 +1,30 @@
+from models.staffrequest import StaffRequest_Control
+from models.staffrequest import StaffRequest
+from clear import clear
+import database
+
+class HumanResourceTeamMemberView:
+    def __init__(self):
+        self.staffRequest_Control= StaffRequest_Control()
+        
+        while(True):
+            clear()
+            print("Human Resource Team Member Main View")
+            print("------------------------")
+            print("[0] Logout")
+            print("[1] Show current staff requests")
+
+            
+            key = input("Please choose key: ")
+            if (key == '0'): break
+            elif (key == '1'): self.show_current_staff_requests()
+            elif(key=="test"):
+                atributes=["Full time", "Decoration" , "4" , "Table fixer" , "fix tables to make them look nice", "opened"]
+                req=StaffRequest(database.id_counter.get_new(),atributes)
+                database.staffRequest_List.append(req)
+        return
+    def show_current_staff_requests(self): 
+        clear()
+        print(*self.staffRequest_Control.get_current_staff_request(),end="")
+        input()
+        
