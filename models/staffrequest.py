3c92b38a7d94d1d8f95b581a9a6cec59107ebbfc
Erik Wendel <ewend@kth.se>
2021-10-23 13:25:01 +0200

    added entity obj and control obj for SR

---
+++ models/staffrequest.py
@@ -0,0 +1,17 @@
+import database
+from clear import clear
+
+class StaffRequest:
+    def __init__(self, id, atributes):
+        self.__id=id
+        self.__contract_type=atributes[0]
+        self.__requesting_department=atributes[1]
+        self.__years_of_experience=atributes[2]
+        self.__job_title=atributes[3]
+        self.__job_description=atributes[4]
+        self.__status=atributes[5]
+
+class StaffRequestControl:
+    def create_staffRequest(self, atributes):
+        e=StaffRequest(id=database.id_counter.get_new(), atributes=atributes)
+        database.staffRequest_List.append(e)
---
+++ tests/test_store_staff_request.py
@@ -0,0 +1,21 @@
+import sys, os
+testdir = os.path.dirname(__file__)
+parentdir = '..'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, parentdir)))
+
+from models.staffrequest import *
+import database
+
+
+
+def main():
+    database.initialize()
+
+    atributes=["Full time", "Decoration" , "4" , "Table fixer" , "fix tables to make them look nice", "opened"]
+
+    req=StaffRequest(database.id_counter.get_new(),atributes)
+    database.staffRequest_List.append(req)
+    if req in database.staffRequest_List: print("it worked!")
+    else: print("it did not work!! :,(")
+
+main()
